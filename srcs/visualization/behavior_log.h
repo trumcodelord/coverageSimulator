0a04acb792353fe5cbd37e1e417b5500f98b9fc5
Hà Kim Minh <gugovscoccoclegendxp@gmail.com>
2026-05-23 09:14:50 +0700

    Make behavior logging header-only for build robustness

--- srcs/visualization/behavior_log.h
+++ srcs/visualization/behavior_log.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "hud_renderer.h"
+
+#include <iostream>
 #include <string>
 
-void logBehavior(const std::string &message);
+inline void logBehavior(const std::string &message)
+{
+    std::cout << message << '\n';
+    pushHUDEvent(message);
+}
