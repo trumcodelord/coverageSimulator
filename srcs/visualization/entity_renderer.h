a68166633b6779cd6fc8b3a2bc89e4e05a33677f
Hà Kim Minh <gugovscoccoclegendxp@gmail.com>
2026-05-26 09:04:54 +0700

    Render robot from core pending move

--- srcs/visualization/entity_renderer.h
+++ srcs/visualization/entity_renderer.h
@@ -1,16 +1,15 @@
 #pragma once
 
+#include "coverage_context.h"
 #include "types.h"
 
 #include <opencv2/core.hpp>
 
 void paintBase(cv::Mat &canvas, const Robot &rb);
 
-void paintRobot(cv::Mat &canvas, const Robot &rb, RobotMode mode);
+void paintRobot(cv::Mat &canvas, const Robot &rb, const CoverageContext &ctx);
 
 void paintPath(cv::Mat &canvas, const Robot &rb);
-
-void paintVisualCoverage(cv::Mat &canvas);
 
 void paintTrail(cv::Mat &canvas, const Robot &rb);
 
