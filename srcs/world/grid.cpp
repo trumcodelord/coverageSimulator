1f5ab8567c53d3f6ce871fbc9774666a81ec03a8
Hà Kim Minh <gugovscoccoclegendxp@gmail.com>
2026-05-26 07:20:24 +0700

    Add terrain cost grid storage

--- srcs/world/grid.cpp
+++ srcs/world/grid.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int rows, cols;
 Cell start;
 bool blocked[1001][1001], covered[1001][1001], dynamicBlocked[1001][1001];
+int terrainCost[1001][1001];
 const int dr[5] = {0, 1, 0, -1, 0};
 const int dc[5] = {0, 0, 1, 0, -1};
 int initialFreeCells = 0;
@@ -24,6 +25,13 @@ bool isCovered(int r, int c)
     return inBounds(r, c) && covered[r][c];
 }
 
+int terrainCostAt(int r, int c)
+{
+    if (!inBounds(r, c))
+        return INF;
+
+    return terrainCost[r][c];
+}
 
 static bool isCoverageTargetCell(int r, int c)
 {
