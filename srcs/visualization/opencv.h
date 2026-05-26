6205878d7bf4387921fc35115f10b2bff0919542
Hà Kim Minh <gugovscoccoclegendxp@gmail.com>
2026-05-26 09:05:55 +0700

    Pass coverage context to drawFrame

--- srcs/visualization/opencv.h
+++ srcs/visualization/opencv.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "coverage_context.h"
 #include "types.h"
 
 #include <string>
 
 void initWindow();
 void closeWindow();
-void drawFrame(const Robot &rb, bool showPath, int delay, RobotMode mode = NORMAL);
+void drawFrame(const Robot &rb, const CoverageContext &ctx, bool showPath, int delay);
 void waitFrame(int delay);
 
 bool saveCurrentFrame(const std::string &filename);
